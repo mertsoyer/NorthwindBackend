aff1487f1949d7132f0b379ea86a8094c337ba57
Mert Soyer <mertsoyer@hotmail.com.tr>
2022-09-23 13:37:35 +0300

    dependency injection için autofac yapısı kuruldu

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule:Module
+    {
+        /// <summary>
+        /// Dependency Injection Autofac ile kurgulanması
+        /// </summary>
+        /// <param name="builder"></param>
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductService>().As<IProductService>();
+            builder.RegisterType<EfProductDal>().As<IProductDal>();
+        }
+
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,13 @@
-var builder = WebApplication.CreateBuilder(args);
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Business.DependencyResolvers.Autofac;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+    .ConfigureContainer<ContainerBuilder>(builder =>
+    {
+        builder.RegisterModule(new AutofacBusinessModule());
+    });
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -8,6 +16,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
